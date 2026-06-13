7b10d0a64a9ab0deba46d9441887d2de99455b8a
R0BC0D3R <R0BC0D3R@gmail.com>
2026-06-13 12:05:58 -0400

    chore: update CLI download links to v0.2.3.0

--- xnv-app/themes/wp/define-download-urls.php
+++ xnv-app/themes/wp/define-download-urls.php
@@ -1,17 +1,17 @@
 <?php 
-define('CLI_VERSION', 'v0.2.2.0');
+define('CLI_VERSION', 'v0.2.3.0');
 define('CLI_CODENAME', 'Legacy Reborn');
 define('GUI_VERSION', 'v1.2.0');
 define('GUI_CODENAME', 'NervaOne');
 define('LEDGER_VERSION', 'v0.1.7.0+');
 
-define('LINUX_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-linux-x86_64-v0.2.2.0.tar.bz2');
-define('LINUX_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-linux-armv8-v0.2.2.0.tar.bz2');
-define('WINDOWS_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-windows-x64-v0.2.2.0.zip');
-define('WINDOWS_CLI_LINK_X32', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-windows-x32-v0.2.2.0.zip');
-define('MAC_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-macos-x64-v0.2.2.0.tar.bz2');
-define('MAC_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-macos-armv8-v0.2.2.0.tar.bz2');
-define('ANDROID_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.2.0/nerva-android-armv8-v0.2.2.0.tar.bz2');
+define('LINUX_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-linux-x86_64-v0.2.3.0.tar.bz2');
+define('LINUX_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-linux-armv8-v0.2.3.0.tar.bz2');
+define('WINDOWS_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-windows-x64-v0.2.3.0.zip');
+define('WINDOWS_CLI_LINK_X32', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-windows-x32-v0.2.3.0.zip');
+define('MAC_CLI_LINK_X64', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-macos-x64-v0.2.3.0.tar.bz2');
+define('MAC_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-macos-armv8-v0.2.3.0.tar.bz2');
+define('ANDROID_CLI_LINK_ARM', 'https://github.com/nerva-project/nerva/releases/download/v0.2.3.0/nerva-android-armv8-v0.2.3.0.tar.bz2');
 
 define('LINUX_GUI_LINK_X64', 'https://github.com/nerva-project/NervaOneWalletMiner/releases/download/v1.2.0/nervaone-v1.2.0-linux-x64.zip');
 define('LINUX_GUI_LINK_ARM64', 'https://github.com/nerva-project/NervaOneWalletMiner/releases/download/v1.2.0/nervaone-v1.2.0-linux-arm64.zip');
