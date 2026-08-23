873a7ea9c2c9cff66facc3492bfa17a1a91d122c
R0BC0D3R <R0BC0D3R@gmail.com>
2026-08-23 18:50:43 -0400

    Stop tracking WordPress core, plugins and runtime files

--- xnv-app/plugins/disable-embeds/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-polyfill'), 'version' => '31e874767ee3032bc02a0262b4ec6fb0');
--- xnv-app/plugins/wp-head-cleaner/uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    die();
-}
-
-delete_option( 'wp_head_cleaner_hooks' );
